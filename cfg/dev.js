24ac4900be825bc0c8a79b356ded10edcf096c16
Austin Bourdier <austinbourdier@compology.us>
2017-03-30 22:14:30 -0700

    adds all dependencies to reg dependencies

--- cfg/dev.js
+++ cfg/dev.js
@@ -29,12 +29,7 @@ let config = Object.assign({}, baseConfig, {
 // Add needed loaders to the defaults here
 config.module.loaders.push({
   test: /\.(js|jsx)$/,
-  loader: 'babel-loader',
-  resolveLoader: {
-    modulesDirectories: [
-    '../node_modules'
-    ]
-  },
+  loader: 'react-hot!babel-loader',
   include: [].concat(
     config.additionalPaths,
     [ path.join(__dirname, '/../src') ]
