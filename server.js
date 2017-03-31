9fccc4c1aa6d31edeb520832cc37435a2ae2a844
Austin Bourdier <austinbourdier@compology.us>
2017-03-30 22:23:41 -0700

    adds all dependencies to reg dependencies

--- server.js
+++ server.js
@@ -13,17 +13,8 @@ const open = require('open');
 let isInitialCompilation = true;
 
 const compiler = webpack(config);
-console.log('TESTING')
-console.log('TESTING')
-console.log('TESTING')
-console.log('TESTING')
-console.log(config)
-console.log('TESTING')
-console.log('TESTING')
-console.log('TESTING')
-console.log('TESTING')
 new WebpackDevServer(compiler, config.devServer)
-.listen(process.env.PORT || config.port, 'localhost', (err) => {
+.listen(onfig.port, 'localhost', (err) => {
   if (err) {
     console.log(err);
   }
@@ -35,6 +26,15 @@ compiler.plugin('done', () => {
     // Ensures that we log after webpack printed its stats (is there a better way?)
     setTimeout(() => {
       console.log('\n✓ The bundle is now ready for serving!\n');
+      console.log('TESTING')
+      console.log('TESTING')
+      console.log('TESTING')
+      console.log('TESTING')
+      console.log(config)
+      console.log('TESTING')
+      console.log('TESTING')
+      console.log('TESTING')
+      console.log('TESTING')
       console.log('  Open in iframe mode:\t\x1b[33m%s\x1b[0m',  'http://localhost:' + config.port + '/webpack-dev-server/');
       console.log('  Open in inline mode:\t\x1b[33m%s\x1b[0m', 'http://localhost:' + config.port + '/\n');
       console.log('  \x1b[33mHMR is active\x1b[0m. The bundle will automatically rebuild and live-update on changes.')
