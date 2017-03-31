38e003395ff5fe81d245fe223e0423f6cc2006b6
Austin Bourdier <austinbourdier@compology.us>
2017-03-31 00:08:36 -0700

    going to merge scotts stuff

--- server.js
+++ server.js
@@ -5,40 +5,50 @@ const webpack = require('webpack');
 const WebpackDevServer = require('webpack-dev-server');
 const config = require('./webpack.config');
 const open = require('open');
+const express = require('express');
+const app = express();
 
 /**
  * Flag indicating whether webpack compiled for the first time.
  * @type {boolean}
  */
-let isInitialCompilation = true;
+// let isInitialCompilation = true;
 
-const compiler = webpack(config);
-new WebpackDevServer(compiler, config.devServer)
-.listen(config.port, 'localhost', (err) => {
-  if (err) {
-    console.log(err);
-  }
-  console.log('Listening at localhost:' + config.port);
-});
+// const compiler = webpack(config);
+// new WebpackDevServer(compiler, config.devServer)
+// .listen(config.port, 'localhost', (err) => {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.log('Listening at localhost:' + config.port);
+// });
 
-compiler.plugin('done', () => {
-  if (isInitialCompilation) {
-    // Ensures that we log after webpack printed its stats (is there a better way?)
-    setTimeout(() => {
-      console.log('\n✓ The bundle is now ready for serving!\n');
-      console.log('TESTING')
-      console.log('TESTING')
-      console.log('TESTING')
-      console.log('TESTING')
-      console.log(config)
-      console.log('TESTING')
-      console.log('TESTING')
-      console.log('TESTING')
-      console.log('TESTING')
-      console.log('  Open in iframe mode:\t\x1b[33m%s\x1b[0m',  'http://localhost:' + config.port + '/webpack-dev-server/');
-      console.log('  Open in inline mode:\t\x1b[33m%s\x1b[0m', 'http://localhost:' + config.port + '/\n');
-      console.log('  \x1b[33mHMR is active\x1b[0m. The bundle will automatically rebuild and live-update on changes.')
-    }, 350);
-  }
-  isInitialCompilation = false;
-});
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+app.listen(8000, function () {
+  console.log('Example app listening on port 8000!')
+})
+// compiler.plugin('done', () => {
+//   if (isInitialCompilation) {
+//     // Ensures that we log after webpack printed its stats (is there a better way?)
+//     setTimeout(() => {
+//       console.log('\n✓ The bundle is now ready for serving!\n');
+//       console.log('TESTING')
+//       console.log('TESTING')
+//       console.log('TESTING')
+//       console.log('TESTING')
+//       console.log(config)
+//       console.log('TESTING')
+//       console.log('TESTING')
+//       console.log('TESTING')
+//       console.log('TESTING')
+//       console.log('  Open in iframe mode:\t\x1b[33m%s\x1b[0m',  'http://localhost:' + config.port + '/webpack-dev-server/');
+//       console.log('  Open in inline mode:\t\x1b[33m%s\x1b[0m', 'http://localhost:' + config.port + '/\n');
+//       console.log('  \x1b[33mHMR is active\x1b[0m. The bundle will automatically rebuild and live-update on changes.')
+//     }, 350);
+//   }
+//   isInitialCompilation = false;
+// });
